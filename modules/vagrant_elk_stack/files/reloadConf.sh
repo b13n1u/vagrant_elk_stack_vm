b8b02602ad4863f0144847953d3acbb83e26f0f6
bieniu <marcin.bienek@redknee.com>
2014-09-04 11:02:25 +0200

    init stupid woa

---
+++ modules/vagrant_elk_stack/files/reloadConf.sh
@@ -0,0 +1,1 @@
+puppet apply --modulepath /tmp/vagrant-puppet-3/modules-0:/etc/puppet/modules --manifestdir /tmp/vagrant-puppet-3/manifests --detailed-exitcodes /tmp/vagrant-puppet-3/manifests/default.pp
--- modules/vagrant_elk_stack/manifests/init.pp
+++ modules/vagrant_elk_stack/manifests/init.pp
@@ -7,11 +7,33 @@ include vagrant_elk_stack::kiban
 include apt 
 
 #pkgs which may be needed
-$basic_pkgs = [ "screen", "strace", "sudo", "htop", "vim", "python-pip", "apache2", "curl" ]
+$basic_pkgs = [ "screen", "strace", "sudo", "htop", "vim", "python-pip", "apache2", "curl", "tmux" ]
 #"libaugeas-ruby", "augeas-tools", "libaugeas-dev" ]
 package { $basic_pkgs:
   ensure => "installed",
 }
+
+file { '/root/startLS.sh': 
+  ensure => file, 
+  content => 'GEM_HOME="/opt/logstash/vendor/bundle/jruby/1.9/" /usr/bin/java -Djava.io.tmpdir=/var/lib/logstash -Xmx500m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -Djava.awt.headless=true -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -jar /opt/logstash/vendor/jar/jruby-complete-1.7.11.jar  -I/opt/logstash/lib /opt/logstash/lib/logstash/runner.rb agent -f /etc/logstash/conf.d -l /var/log/logstash/logstash.log',
+  mode => 775,
+}
+
+file { '/root/reloadConf.sh': 
+  ensure => file, 
+  content => 'puppet apply --modulepath /tmp/vagrant-puppet-3/modules-0:/etc/puppet/modules --manifestdir /tmp/vagrant-puppet-3/manifests --detailed-exitcodes /tmp/vagrant-puppet-3/manifests/default.pp',
+  mode => 775,
+}
+
+
+file {'/etc/tmux.conf': 
+  ensure => file,
+  source => 'puppet:///modules/vagrant_elk_stack/tmux.conf',
+  
+}
+
+ 
+
 
 }#END
 
